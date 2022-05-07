1ea1aea47979733c150eae58a9a759ca92eabd9b
Daniel D <66746948+ensn@users.noreply.github.com>
2022-05-07 16:55:41 +0200

    Add files via upload

---
+++ commonsettings.py
@@ -0,0 +1,79 @@
+clicks=(                #windowed, small GUI
+    (950, 392),         #save and quit
+    (950, 330),         #singleplayer
+    (1040, 990),        #create new world
+    (950, 200),         #more world options (set seed)
+    (950, 90),          #seed box (set seed)
+    (875, 1010))        #create new world
+
+clicks=(                #fullscreen/borderless, small GUI
+    (950, 388),         #save and quit
+    (950, 325),         #singleplayer
+    (1040, 1040),       #create new world
+    (950, 180),         #more world options (set seed)
+    (950, 70),          #seed box (set seed)
+    (875, 1060))        #create new world
+
+clicks=(                #windowed, normal GUI
+    (950, 510),         #save and quit
+    (950, 390),         #singleplayer
+    (1110, 950),        #create new world
+    (950, 390),         #more world options (set seed)
+    (950, 160),         #seed box (set seed)
+    (800, 995))         #create new world
+
+clicks=(                #fullscreen/borderless, normal GUI
+    (950, 505),         #save and quit
+    (950, 390),         #singleplayer
+    (1110, 1000),       #create new world
+    (950, 365),         #more world options (set seed)
+    (950, 140),         #seed box (set seed)
+    (800, 1045))        #create new world
+
+clicks=(                #windowed, large GUI
+    (950, 623),         #save and quit
+    (950, 450),         #singleplayer
+    (1200, 900),        #create new world
+    (950, 570),         #more world options (set seed)
+    (950, 230),         #seed box (set seed)
+    (700, 970))         #create new world
+
+clicks=(                #fullscreen/borderless, large GUI
+    (950, 623),         #save and quit
+    (950, 450),         #singleplayer
+    (1200, 950),        #create new world
+    (950, 570),         #more world options (set seed)
+    (950, 230),         #seed box (set seed)
+    (700, 1020))        #create new world
+
+clicks=(                #windowed, auto GUI
+    (950, 750),         #save and quit
+    (950, 500),         #singleplayer
+    (1300, 870),        #create new world
+    (950, 750),         #more world options (set seed)
+    (950, 300),         #seed box (set seed)
+    (650, 960))         #create new world
+
+clicks=(                #fullscreen/borderless, auto GUI
+    (950, 740),         #save and quit
+    (950, 507),         #singleplayer
+    (1300, 910),        #create new world
+    (950, 730),         #more world options (set seed)
+    (950, 280),         #seed box (set seed)
+    (650, 1010))        #create new world
+
+pixeldata=(             #for visual_cue setting
+    (1918, 24),         #position (top right, windowed)
+    (46, 33, 23))       #rgb colour
+
+pixeldata=(             #for visual_cue setting
+    (1918, 2),          #position (top right, fullscreen/borderless)
+    (46, 33, 23))       #rgb colour
+
+pixeldata=(             #for visual_cue setting
+    (2, 25),            #position (top left, windowed)
+    (46, 33, 23))       #rgb colour
+
+pixeldata=(             #for visual_cue setting
+    (2, 2),             #position (top left, fullscreen/borderless)
+    (46, 33, 23))       #rgb colour
