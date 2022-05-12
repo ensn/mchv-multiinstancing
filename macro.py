40fa39bcd7d49813cd18de9403ca76ac35229106
Daniel D <66746948+ensn@users.noreply.github.com>
2022-05-12 15:25:06 +0200

    Add files via upload

--- macro.py
+++ macro.py
@@ -1,5 +1,5 @@
 global mouse, keys, focusedInstance
-import keyboard, time, os, settings
+import keyboard, time, settings
 import pynput.mouse as pynm
 import pynput.keyboard as pynk
 try:
@@ -35,17 +35,17 @@ def focusWindow(indx, window):
 def click(pos):
     global mouse
     mouse.position = (pos[0], pos[1])
-    time.sleep(settings.delays[14])
+    time.sleep(settings.delays[13])
     mouse.press(pynm.Button.left)
-    time.sleep(settings.delays[15])
+    time.sleep(settings.delays[14])
     mouse.release(pynm.Button.left)
 
 def send(x):
     global keys
     keys.press(x)
-    time.sleep(settings.delays[16])
+    time.sleep(settings.delays[15])
     keys.release(x)
-    time.sleep(settings.delays[17])
+    time.sleep(settings.delays[16])
 
 
 def reset(minecrafts):
@@ -61,44 +61,41 @@ def reset(minecrafts):
         
     elif settings.visual_cue:
         time.sleep(settings.delays[4])
-        while pyautogui.screenshot(region=settings.pixeldata[0]+(1,1)).getcolors()[0][1]==settings.pixeldata[1]:
+        while pyautogui.screenshot(region=settings.pixeldata[0]+(1,1)).getcolors()[0][1]!=settings.pixeldata[1]:
             continue
-        
+
     else:
         time.sleep(settings.delays[1])
         
     click(settings.clicks[1])           #singleplayer
     time.sleep(settings.delays[5])
     click(settings.clicks[2])           #create new world
-    
     if settings.seed!=0:                    #SS
         time.sleep(settings.delays[7])
-        click(settings.clicks[3])       #more world options
+        click(settings.clicks[3])       #seed box
         time.sleep(settings.delays[8])
-        click(settings.clicks[4])       #seed box
-        time.sleep(settings.delays[9])
         if settings.seed_clipboard:
             keys.press(pynk.Key.ctrl)
-            time.sleep(settings.delays[16])
+            time.sleep(settings.delays[15])
             send("v")
             keys.release(pynk.Key.ctrl)
         else:
             for char in str(settings.seed):
                 send(char)
-        time.sleep(settings.delays[10])
+        time.sleep(settings.delays[9])
         
     else:                                   #RS
         time.sleep(settings.delays[6])
         
-    click(settings.clicks[5])           #create new world
-    time.sleep(settings.delays[11])
+    click(settings.clicks[4])           #create new world
+    time.sleep(settings.delays[10])
     focusedInstance=(focusedInstance+1)%len(minecrafts)
     focusWindow(focusedInstance, minecrafts)
-    time.sleep(settings.delays[12])
+    time.sleep(settings.delays[11])
     send(pynk.Key.esc)                  #unpause
     
     if settings.timer:
-        time.sleep(settings.delays[13])
+        time.sleep(settings.delays[12])
         send(settings.timer_start)
 
       
@@ -115,7 +112,7 @@ class GameMacro:
                 send(this.action)
                 this.step += 1
         if this.step==1:
-            if time.perf_counter() > this.start_time_sec + settings.delays[18]:
+            if time.perf_counter() > this.start_time_sec + settings.delays[17]:
                 this.step=0
 
 
